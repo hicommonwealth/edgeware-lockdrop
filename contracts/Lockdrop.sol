38ae2675771e7bd3936472f9bb251643d49fbc32
Drew Stone <drewstone329@gmail.com>
2019-02-18 23:34:10 +0200

    Adding signalling

--- contracts/Lockdrop.sol
+++ contracts/Lockdrop.sol
@@ -35,6 +35,7 @@ contract Lockdrop {
     uint256 public LOCK_END_TIME;
     
     event Locked(address indexed owner, uint256 eth, Lock lockAddr, Term term, bytes edgewareKey, bool isValidator);
+    event Signalled(address indexed contract, bytes edgewareKey, bool isValidator);
     
     constructor(uint startTime) public {
         LOCK_START_TIME = startTime;
@@ -62,5 +63,34 @@ contract Lockdrop {
         if (term == Term.TwelveMo) return LOCK_START_TIME + LOCK_DROP_PERIOD + 365 days;
         
         revert();
+    }
+
+    function signalWithContract(address who, uint256 nonce, bytes calldata edgewareKey, bool isValidator) public didCreate(who, msg.sender, nonce) {
+        require(now >= LOCK_START_TIME);
+        require(now <= LOCK_END_TIME);
+        emit Signalled(who, pubkey, isValidator);
+    }
+
+    modifier didCreate(address target, address parent, uint256 nonce) {
+        uint32 nonce_len = nonce < 128 ? 1 : nonce < 256 ? 2 : 3;
+        bytes memory b = new bytes(nonce_len + 22);
+        b[0] = bytes1(0xd6 + 21 + nonce_len);
+        b[1] = 0x94;
+        for (uint i = 0; i < 20; ++i) {
+            b[i + 2] = bytes20(parent)[i];
+        }
+        if (nonce_len == 1) {
+            b[22] = nonce == 0 ? bytes1(0x80) : bytes1(nonce);
+        } else {
+            b[22] = bytes1(0x7f + nonce_len);
+            if (nonce_len == 2) {
+                b[23] = bytes1(nonce);
+            } else { // nonce_len == 3
+                b[23] = bytes1(nonce / 256);
+                b[24] = bytes1(nonce % 256);
+            }
+        }
+        require(uint256(target) == uint256(keccak256(b)) & (1 << 20 - 1));
+        _;
     }
 }
